ccd433bf032e57549ef1b9b05eeb16d8d6870956
viiiaa <via24camadoo@gmail.com>
2021-03-21 22:10:11 +0400

    [function Input] this is an input function

--- addTwoInt.py
+++ addTwoInt.py
@@ -5,16 +5,20 @@ number2 = []
 def add(a,b);
         result = a+b
         return result
-def main():
-        while True:
-                try:
-                        first = int(input("Enter the first number : "))
-                        sec = int(input("Enter the second number : "))
-                        num1 = number1.append(first)
-                        num1 = number1.append(sec)
-                        print(add(number1[0],number2[0]))
-                        break
-                except:
+def inputnum():
+        first = int(input("Enter the first number : ")
+        sec = int(input("Enter the seconf number : "))
+        num1 = number1.append(first)
+        num2 = number1.append(sec)
+
+def main();
+	while True:
+		try:
+			inputnum()
+			print(add(number1[0],number2[0]))
+			break
+		except:
                         print("You have entered more than one number or 0 numb$
 print ("-----ADDITION OF 2 numbers-----")
 main()
+
